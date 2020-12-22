97ebc68d70b731ce5747aba3e22ed4906de929b4
Winsor Hoang <trohoang@yahoo.com>
2020-12-22 16:46:27 -0700

    final day 10

--- day_10_calculator.py
+++ day_10_calculator.py
@@ -1,3 +1,7 @@
+from day_10_art import logo
+import os
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -11,27 +15,47 @@ def div(n1, n2):
 
 
 def mult(n1, n2):
-    return n1 + n2
+    return n1 * n2
 
 
-def cal_fun(cal_in, n1, n2):
-    cal_in(n1, n2)
+operations = {"+": add, "-": sub, "*": mult, "/": div}
 
 
-operations = {"+": "add", "-": "sub", "*": "mult", "/": "div"}
-
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
-
-for symbol in operations:
-    print(symbol)
-
-operations_symbol = input("Pick an operation from the lines above: ")
-
-
-cal = operations[operations_symbol]
-
-answer = cal_fun(cal, num1, num2)
+def screen_clear():
+    # for mac and linux(here, os.name is 'posix')
+    if os.name == "posix":
+        _ = os.system("clear")
+    else:
+        # for windows platfrom
+        _ = os.system("cls")
+    # print out some text
 
 
-print(f"{num1} {operations_symbol} {num2} = {answer}")
+def calculator():  # recursion when the function called itself
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+
+    stop_cal = False
+
+    while stop_cal == False:
+        operations_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operations_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operations_symbol} {num2} = {answer}")
+        continue_cal = input(
+            f"Type 'y' to continue to calculate with {answer}, or type 'n' to start a new calculation: "
+        )
+
+        if continue_cal == "n":
+            stop_cal = True
+            screen_clear()
+            calculator()
+        else:
+            num1 = answer
+
+
+calculator()
